855884c4935f0b4cdae1b4abb99f7020ba4c0987
takamal <crossroads22@auone.jp>
2012-09-27 19:46:19 +0900

    yarimasitayoCommit

--- gen/sample/application/fingerpaint1/R.java
+++ gen/sample/application/fingerpaint1/R.java
@@ -24,9 +24,9 @@ public final class R {
         public static final int button1=0x7f080001;
         public static final int imageView1=0x7f080000;
         public static final int linearLayout1=0x7f080002;
-        public static final int menu_color_change=0x7f080005;
+        public static final int menu_color_change=0x7f080006;
         public static final int menu_eraser=0x7f080007;
-        public static final int menu_new=0x7f080006;
+        public static final int menu_new=0x7f080005;
         public static final int menu_open=0x7f080004;
         public static final int menu_save=0x7f080003;
     }
